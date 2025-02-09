ffcb38cb78f3d470a0213495d33907fff4cdaa1c
Daniel Tran <68958503+Uteki@users.noreply.github.com>
2025-02-10 00:59:11 +0100

    update to dish array

--- scripts/data.js
+++ scripts/data.js
@@ -1,8 +1,108 @@
-let orderDishes = [
-    {
-        "dish": "spageitt",
-        "price": "20",
-        "description": "Bestellapps spageitt",
-    //     "number?"
-    },
-]
+let orderMenu = {
+    "mainDishes": [
+        {
+            "item": "Sushi-Platte",
+            "price": "24",
+            "description": "Frische Auswahl an Sushi mit Lachs, Thunfisch und Avocado."
+        },
+        {
+            "item": "Ramen mit Schweinebauch",
+            "price": "18",
+            "description": "Japanische Nudelsuppe mit zartem Schweinebauch, Ei und Frühlingszwiebeln."
+        },
+        {
+            "item": "Gebratene Ente mit Reis",
+            "price": "22",
+            "description": "Knusprige Ente auf duftendem Jasminreis mit süß-saurer Sauce."
+        },
+        {
+            "item": "Pad Thai mit Garnelen",
+            "price": "19",
+            "description": "Thailändische Reisnudeln mit Garnelen, Erdnüssen und Limette."
+        },
+        {
+            "item": "Koreanisches Bibimbap",
+            "price": "17",
+            "description": "Reisschüssel mit Rindfleisch, Gemüse, Ei und scharfer Gochujang-Sauce."
+        },
+        {
+            "item": "Scharfes Mapo Tofu",
+            "price": "16",
+            "description": "Würziger Seidentofu mit Hackfleisch in pikanter Szechuan-Sauce."
+        },
+        {
+            "item": "Dim Sum Mix",
+            "price": "15",
+            "description": "Eine Auswahl an gedämpften Teigtaschen mit verschiedenen Füllungen."
+        },
+        {
+            "item": "Teriyaki Lachs",
+            "price": "21",
+            "description": "Gegrillter Lachs in Teriyaki-Sauce mit Gemüse und Reis."
+        },
+        {
+            "item": "Grünes Thai-Curry mit Huhn",
+            "price": "18",
+            "description": "Cremiges Kokosnuss-Curry mit Hähnchen, Gemüse und Jasminreis."
+        },
+        {
+            "item": "Gebackene Bananen mit Honig",
+            "price": "9",
+            "description": "Knusprige Bananen mit Honigglasur und Vanilleeis."
+        }
+    ],
+    "sideDishes": [
+        {
+            "item": "Edamame",
+            "price": "6",
+            "description": "Gesalzene grüne Sojabohnen, perfekt als Snack."
+        },
+        {
+            "item": "Gyoza",
+            "price": "8",
+            "description": "Knusprig gebratene Teigtaschen mit Schweinefleischfüllung."
+        },
+        {
+            "item": "Miso-Suppe",
+            "price": "5",
+            "description": "Traditionelle japanische Suppe mit Tofu, Algen und Frühlingszwiebeln."
+        },
+        {
+            "item": "Kimchi",
+            "price": "4",
+            "description": "Fermentierter, scharf gewürzter Chinakohl aus Korea."
+        },
+        {
+            "item": "Frühlingsrollen",
+            "price": "7",
+            "description": "Knusprige Teigrollen gefüllt mit Gemüse und Glasnudeln."
+        }
+    ],
+    "drinks": [
+        {
+            "item": "Grüner Tee",
+            "price": "4",
+            "description": "Traditioneller japanischer grüner Tee, heiß serviert."
+        },
+        {
+            "item": "Mango Lassi",
+            "price": "5",
+            "description": "Cremiges Joghurtgetränk mit frischer Mango."
+        },
+        {
+            "item": "Pflaumenwein",
+            "price": "6",
+            "description": "Süßer japanischer Umeshu-Pflaumenwein, kalt serviert."
+        },
+        {
+            "item": "Sake",
+            "price": "8",
+            "description": "Warmer japanischer Reiswein mit mildem Aroma."
+        },
+        {
+            "item": "Matcha Latte",
+            "price": "5",
+            "description": "Cremiger Latte mit fein gemahlenem Matcha-Tee."
+        }
+    ]
+};
